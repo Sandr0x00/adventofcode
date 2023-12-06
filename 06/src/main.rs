64746b9162ecc281bc19cbaf89993327ff4afbc3
sandr0 <s@sandr0.xyz>
2023-12-06 18:12:38 +0100

    nicer code

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -25,15 +25,33 @@ fn main() {
     let input = fs::read_to_string("input").unwrap();
 
     let mut res_one = 1;
-    if let [times_one, records_one] = &input.lines().map(|l| l[10..].split(' ').filter_map(|w| w.parse::<usize>().ok()).collect::<Vec<_>>()).collect::<Vec<_>>()[0..2] {
-        assert!(times_one.len() == records_one.len());
-        for i in 0..times_one.len() {
-            res_one *= ways_to_win(times_one[i], records_one[i]);
-        }
-    }
-    println!("Part One {}", res_one);
+    let data_one = &input
+        .lines()
+        .map(|l| {
+            l[10..]
+                .split_ascii_whitespace()
+                .map(|w| w.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
-    if let [time_two, record_two] = input.lines().map(|l| l[10..].chars().filter(|c| !c.is_whitespace()).collect::<String>().parse::<usize>().unwrap()).collect::<Vec<_>>()[0..2] {
-        println!("Part Two {}", ways_to_win(time_two, record_two));
+    assert_eq!(data_one[0].len(), data_one[1].len());
+    for i in 0..data_one[0].len() {
+        res_one *= ways_to_win(data_one[0][i], data_one[1][i]);
     }
+
+    let data_two = input
+        .lines()
+        .map(|l| {
+            l[10..]
+                .chars()
+                .filter(|c| !c.is_whitespace())
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    println!("Part One {}", res_one);
+    println!("Part Two {}", ways_to_win(data_two[0], data_two[1]));
 }
