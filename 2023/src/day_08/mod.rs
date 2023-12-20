5bd1f5a809ab8368915163b2067c94c9532e6740
sandr0 <s@sandr0.xyz>
2023-12-20 18:14:20 +0100

    moved lcm to lib

--- 2023/src/day_08/mod.rs
+++ 2023/src/day_08/mod.rs
@@ -2,24 +2,6 @@ use std::collections::HashMap;
 
 const DAY: u8 = 8;
 
-pub fn lcm(nums: &[usize]) -> usize {
-    if nums.len() == 1 {
-        return nums[0];
-    }
-    let a = nums[0];
-    let b = lcm(&nums[1..]);
-
-    a * b / gcd_of_two_numbers(a, b)
-}
-
-fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
-    if b == 0 {
-        return a;
-    }
-
-    gcd_of_two_numbers(b, a % b)
-}
-
 fn traverse(network: &HashMap<&str, (&str, &str)>, instructions: &[char], start: &str, part_two: bool) -> usize {
     let mut i = 0;
     let mut next = start;
@@ -62,7 +44,7 @@ pub fn solve() {
     for start in starts {
         steps.push(traverse(&network, &instructions, start, true));
     }
-    let part_two = lcm(&steps);
+    let part_two = aoc::lcm(&steps);
 
     aoc::print_solution(DAY, &[
         part_one,
